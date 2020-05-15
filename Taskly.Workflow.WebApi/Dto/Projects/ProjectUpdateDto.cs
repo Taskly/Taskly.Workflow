b45b1eeaf4551471c10255a2ed19cd6696ce8d46
Mikhail Doshevsky <m.doshevsky@gmail.com>
2020-05-15 08:11:09 +0300

    Implement update Tasks and Projects

---
+++ Taskly.Workflow.WebApi/Dto/Projects/ProjectUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Taskly.Workflow.WebApi.Dto.WorkItems;
+
+namespace Taskly.Workflow.WebApi.Dto.Projects
+{
+    public class ProjectUpdateDto
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public List<WorkItemStatusDto> AvailableStatuses { get; set; }
+    }
+}
---
+++ Taskly.Workflow.WebApi/Dto/WorkItems/WorkItemUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Taskly.Workflow.WebApi.Dto.WorkItems
+{
+    public class WorkItemUpdateDto
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public WorkItemStatusDto Status { get; set; }
+    }
+}
